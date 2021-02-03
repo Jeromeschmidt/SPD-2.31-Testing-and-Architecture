1fdadb3e7cbaf1aca67c4f6015ccf08d97ed6278
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-03 17:43:33 -0600

    initial commit

--- lab/refactoring-pep8/docstrings_blank_lines.py
+++ lab/refactoring-pep8/docstrings_blank_lines.py
@@ -1,42 +1,84 @@
 # by Kami Bigdely
 # Docstrings and blank lines
+
+
 class OnBoardTemperatureSensor:
+    """Class for On-Board Temperature Sensor"""
+
     VOLTAGE_TO_TEMP_FACTOR = 5.6
+
     def __init__(self):
+        """init function"""
         pass
-    def read_voltage(self):        
+
+    def read_voltage(self):
+        """read voltage"""
         return 2.7
+
     def get_temperature(self):
-        return self.read_voltage() * OnBoardTemperatureSensor.VOLTAGE_TO_TEMP_FACTOR # [celcius]
-  
+        """get temperature"""
+        # [celcius]
+        return self.read_voltage() * \
+        OnBoardTemperatureSensor.VOLTAGE_TO_TEMP_FACTOR
+
+
 class CarbonMonoxideSensor:
+    """Class for Carbon Monoxide Sensor"""
+
     VOLTAGE_TO_CO_FACTOR = 0.048
+
     def __init__(self, temperature_sensor):
+        """init function"""
         self.on_board_temp_sensor = temperature_sensor
         if not self.on_board_temp_sensor:
             self.on_board_temp_sensor = OnBoardTemperatureSensor()
+
     def get_carbon_monoxide_level(self):
+        """get carbon monoxide level"""
         sensor_voltage = self.read_sensor_voltage()
-        self.carbon_monoxide = CarbonMonoxideSensor.convert_voltage_to_carbon_monoxide_level(sensor_voltage, self.on_board_temp_sensor.get_temperature())
+        self.carbon_monoxide = CarbonMonoxideSensor. \
+        convert_voltage_to_carbon_monoxide_level(sensor_voltage,
+                                                 self.on_board_temp_sensor.
+                                                 get_temperature())
         return self.carbon_monoxide
+
     def read_sensor_voltage(self):
-        # In real life, it should read from hardware.        
+        """get sensor voltage"""
+        # In real life, it should read from hardware.
         return 2.3
+
     def convert_voltage_to_carbon_monoxide_level(voltage, temperature):
-        return voltage * CarbonMonoxideSensor.VOLTAGE_TO_CO_FACTOR * temperature
-    
+        """convert voltage to carbon monoxide level"""
+        return voltage * \
+        CarbonMonoxideSensor.VOLTAGE_TO_CO_FACTOR * temperature
+
+
 class DisplayUnit:
+    """Class for Display Unit Sensor"""
+
     def __init__(self):
+        """init function"""
         self.string = ''
-    def display(self,msg):
+
+    def display(self, msg):
+        """display message"""
         print(msg)
+
+
 class CarbonMonoxideDevice():
+    """Class for Carbon Monoxide Device Sensor"""
+
     def __init__(self, co_sensor, display_unit):
-        self.carbonMonoxideSensor = co_sensor 
-        self.display_unit = display_unit       
+        """init function"""
+        self.carbonMonoxideSensor = co_sensor
+        self.display_unit = display_unit
+
     def Display(self):
-        msg = 'Carbon Monoxide Level is : ' +  str(self.carbonMonoxideSensor.get_carbon_monoxide_level())
+        """send message to display to display function"""
+        msg = 'Carbon Monoxide Level is : ' + \
+        str(self.carbonMonoxideSensor.get_carbon_monoxide_level())
         self.display_unit.display(msg)
+
 
 if __name__ == "__main__":
     temp_sensor = OnBoardTemperatureSensor()
@@ -44,4 +86,3 @@ if __name__ == "__main__":
     display_unit = DisplayUnit()
     co_device = CarbonMonoxideDevice(co_sensor, display_unit)
     co_device.Display()
-    
--- lab/refactoring-pep8/whitespace_naming_convention2.py
+++ lab/refactoring-pep8/whitespace_naming_convention2.py
@@ -1,32 +1,40 @@
 # By Kami Bigdely
 # PEP8 - whitespaces and variable names.
+
+
 class pizza:
-    def __init__ (obj, mybread_type,CHEESE_TYPE,meatType,pizza_toppings,size):
-        obj.bread_type= mybread_type
-        obj.cheese_type = CHEESE_TYPE
-        obj.meatType= meatType
+    def __init__(obj, bread_type, cheese_type,
+                 meat_type, pizza_toppings, size):
+        obj.bread_type = bread_type
+        obj.cheese_type = cheese_type
+        obj.meat_type = meat_type
         obj.toppings = pizza_toppings
-        obj.size = size        
+        obj.size = size
+
     @classmethod
-    def Create_ChicagoPizza (class_type, size):
+    def Create_ChicagoPizza(class_type, size):
         bread = 'deep-dish bread'
         cheese = 'mozzarella cheese'
-        meatType= 'Italian sausage'
-        toppings = ['green bell pepper','mushroom', 'chunky tomato sauce', 'onion']
-        return class_type (bread, cheese, meatType, toppings, size)    
+        meat_type = 'Italian sausage'
+        toppings = ['green bell pepper', 'mushroom',
+                    'chunky tomato sauce', 'onion']
+        return class_type(bread, cheese, meat_type, toppings, size)
+
     @classmethod
-    def createCalifornia_pizza(ct, meat_Type,size):
+    def createCalifornia_pizza(ct, meat_type, size):
         bread = 'thin crust'
-        CHEESE = 'feta cheese'
-        toppings =[ 'garlic', 'spinach', 'broccoli', 'olives', 'red onion', 'red bell pepper' ]
-        return ct(bread, CHEESE, meat_Type, toppings, size) 
+        cheese = 'feta cheese'
+        toppings = ['garlic', 'spinach', 'broccoli', 'olives',
+                    'red onion', 'red bell pepper']
+        return ct(bread, cheese, meat_type, toppings, size)
+
     def printInfo(obj):
         print('bread type is: ', obj.bread_type)
         print('cheese type is: ', obj.cheese_type)
-        print('meat type is: ', obj.meatType)
+        print('meat type is: ', obj.meat_type)
         print('Toppings are: ', end='')
         print(', '.join(map(str, obj.toppings)))
 
-    
+
 myPizza = pizza.createCalifornia_pizza('chicken', 'large')
 myPizza.printInfo()
